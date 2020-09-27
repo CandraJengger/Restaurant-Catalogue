e021a52a916406ab996c2f36ac84b35bef7af1ff
CandraJengger <cakejengger@gmail.com>
2020-09-27 22:15:41 +0700

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,25 @@
 import 'regenerator-runtime' /* for async await transpile */
 import '../styles/main.scss'
 
+import '../scripts/views/templates/NavBar'
+import '../scripts/views/templates/ButtonComp'
+import '../scripts/views/templates/FooterComp'
+
+import App from './views/app'
+
+const app = new App({
+  button: document.getElementById('btn-tab-sidebar'),
+  drawer: document.getElementById('navbar'),
+  content: document.querySelector('main'),
+  layer: document.querySelector('.layer')
+})
+
+window.addEventListener('hashchange', () => {
+  app.renderPage()
+})
+
+window.addEventListener('load', () => {
+  app.renderPage()
+})
+
 console.log('Hello Coders! :)')
