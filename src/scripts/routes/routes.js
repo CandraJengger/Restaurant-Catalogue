fbdca9ea55c12646928e4ec6313e1e62e39c1ab5
CandraJengger <cakejengger@gmail.com>
2020-10-01 07:39:21 +0700

    Update routes

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,8 +1,10 @@
 import Home from '../views/pages/home'
+import Favorite from '../views/pages/favorite'
 
 const routes = {
   '/': Home,
-  '/home': Home
+  '/home': Home,
+  '/favorite': Favorite
 }
 
 export default routes
