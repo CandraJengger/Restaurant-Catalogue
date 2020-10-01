290395b942fb7155f889aba8df6eec3729d74abb
CandraJengger <cakejengger@gmail.com>
2020-10-01 07:40:45 +0700

    Add favorite page

---
+++ src/scripts/views/pages/favorite.js
@@ -0,0 +1,15 @@
+const Favorite = {
+  async render () {
+    return `
+      <div id="content">
+        <h2>Your Favorite Restaurant</h2>
+      </div>
+    `
+  },
+
+  async afterRender () {
+
+  }
+}
+
+export default Favorite
