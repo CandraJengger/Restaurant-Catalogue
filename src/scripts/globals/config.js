bb24709d0ffa9a3204f0a3465a23817773c2e767
CandraJengger <cakejengger@gmail.com>
2020-10-05 14:21:36 +0700

    Update globals

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,7 @@
 const CONFIG = {
   BASE_URL: 'https://dicoding-restaurant-api.el.r.appspot.com/',
-  BASE_IMAGE_URL: 'https://dicoding-restaurant-api.el.r.appspot.com/images/medium/'
+  BASE_IMAGE_URL_MEDIUM: 'https://dicoding-restaurant-api.el.r.appspot.com/images/medium/',
+  BASE_IMAGE_URL_LARGE: 'https://dicoding-restaurant-api.el.r.appspot.com/images/medium/'
 }
 
 export default CONFIG
