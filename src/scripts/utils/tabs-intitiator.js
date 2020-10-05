3aaa0d843878bb3d626fe4c64854ca1cf91a0112
CandraJengger <cakejengger@gmail.com>
2020-10-05 14:19:01 +0700

    Add tabs-intitiator.js

---
+++ src/scripts/utils/tabs-intitiator.js
@@ -0,0 +1,37 @@
+const TabsIntitiator = {
+  init ({ tabLinks, tabViews }) {
+    tabLinks.forEach(tab => {
+      tab.addEventListener('click', event => {
+        tabLinks.forEach(tab => {
+          this._removeActiveTab(tab)
+        })
+        this._addActiveTab(event.target)
+        
+        tabViews.forEach(tabView => {
+          this._remoceActiveTabView(tabView)
+          if (tabView.getAttribute('id') === event.target.textContent.toLowerCase()) {
+            this._addActiveTabView(tabView)
+          }
+        })
+      })
+    })
+  },
+
+  _addActiveTabView (tabView) {
+    tabView.classList.add('active-tab-view')
+  },
+
+  _remoceActiveTabView (tabView) {
+    tabView.classList.remove('active-tab-view')
+  },
+
+  _addActiveTab (tab) {
+    tab.classList.add('active-tab')
+  },
+
+  _removeActiveTab (tab) {
+    tab.classList.remove('active-tab')
+  }
+}
+
+export default TabsIntitiator
