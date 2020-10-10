657769aab2ecb5bd9aafa7ee643b9c129b9363f4
CandraJengger <cakejengger@gmail.com>
2020-10-10 21:20:59 +0700

    Update globals: api-endpoint and config

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -2,7 +2,8 @@ import CONFIG from './config'
 
 const API_ENDPOINT = {
   LIST_RESTAURANT: `${CONFIG.BASE_URL}list`,
-  DETAIL: id => `${CONFIG.BASE_URL}detail/${id}`
+  DETAIL: id => `${CONFIG.BASE_URL}detail/${id}`,
+  SEARCH_RESTAURANT: query => `${CONFIG.BASE_URL}/search?q=${query}`
 }
 
 export default API_ENDPOINT
--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,7 +1,11 @@
 const CONFIG = {
   BASE_URL: 'https://dicoding-restaurant-api.el.r.appspot.com/',
   BASE_IMAGE_URL_MEDIUM: 'https://dicoding-restaurant-api.el.r.appspot.com/images/medium/',
-  BASE_IMAGE_URL_LARGE: 'https://dicoding-restaurant-api.el.r.appspot.com/images/medium/'
+  BASE_IMAGE_URL_LARGE: 'https://dicoding-restaurant-api.el.r.appspot.com/images/medium/',
+  CACHE_NAME: new Date().toISOString(),
+  DATABASE_NAME: 'meluwe-db',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'movies'
 }
 
 export default CONFIG
