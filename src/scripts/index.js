f8420734fd6e96ce63132303a91ae7ef912d00fe
CandraJengger <cakejengger@gmail.com>
2020-10-10 21:25:22 +0700

    Update index.js : add loading, serviceWorker register and properti in object App

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,15 +4,19 @@ import '../styles/main.scss'
 import '../scripts/views/templates/NavBar'
 import '../scripts/views/templates/ButtonComp'
 import '../scripts/views/templates/FooterComp'
-
+import '../scripts/views/templates/SpinnerComp'
+import '../scripts/views/templates/ErrorComp'
+import '../scripts/views/templates/ButtonIconComp'
 import App from './views/app'
+import swRegister from '../scripts/utils/sw-register'
 
 const app = new App({
-  btnSidebar: document.querySelector('#btn-tab-sidebar'),
+  btnSidebar: document.querySelector('.btn-tab-sidebar'),
   btnSkip: document.querySelector('.btn-skip'),
   drawer: document.querySelector('#navbar'),
   content: document.querySelector('main'),
-  layer: document.querySelector('.layer')
+  layer: document.querySelector('.layer'),
+  links: document.querySelectorAll('.nav-links li a')
 })
 
 window.addEventListener('hashchange', () => {
@@ -21,6 +25,5 @@ window.addEventListener('hashchange', () => {
 
 window.addEventListener('load', () => {
   app.renderPage()
+  swRegister()
 })
-
-console.log('Hello Coders! :)')
