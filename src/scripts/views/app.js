0c3244511143ccc3de0405c16dc993af16578f5e
CandraJengger <cakejengger@gmail.com>
2020-10-10 21:27:02 +0700

    Update app.js : add links variable

--- src/scripts/views/app.js
+++ src/scripts/views/app.js
@@ -3,12 +3,13 @@ import routes from '../routes/routes'
 import UrlParser from '../routes/url-parser'
 
 class App {
-  constructor ({ btnSidebar, btnSkip, drawer, content, layer }) {
+  constructor ({ btnSidebar, btnSkip, drawer, content, layer, links }) {
     this._btnSidebar = btnSidebar
     this._btnSkip = btnSkip
     this._layer = layer
     this._drawer = drawer
     this._content = content
+    this._links = links
 
     this._initialAppShell()
   }
@@ -19,12 +20,13 @@ class App {
       btnSkip: this._btnSkip,
       drawer: this._drawer,
       content: this._content,
-      layer: this._layer
+      layer: this._layer,
+      links: this._links
     })
   }
 
   async renderPage () {
-    const url = UrlParser.parserActiveUrlWithCombiner()
+    const url = UrlParser.parseActiveUrlWithCombiner()
     const page = routes[url]
     this._content.innerHTML = await page.render()
     await page.afterRender()
