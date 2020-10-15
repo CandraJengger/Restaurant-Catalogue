c24f3526cb79304a4909bbb226cf77f16b620e46
CandraJengger <cakejengger@gmail.com>
2020-10-15 21:10:23 +0700

    Update api-endpoint.js: add POST_REVIEW

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -3,7 +3,8 @@ import CONFIG from './config'
 const API_ENDPOINT = {
   LIST_RESTAURANT: `${CONFIG.BASE_URL}list`,
   DETAIL: id => `${CONFIG.BASE_URL}detail/${id}`,
-  SEARCH_RESTAURANT: query => `${CONFIG.BASE_URL}/search?q=${query}`
+  SEARCH_RESTAURANT: query => `${CONFIG.BASE_URL}/search?q=${query}`,
+  POST_REVIEW: `${CONFIG.BASE_URL}review`
 }
 
 export default API_ENDPOINT
